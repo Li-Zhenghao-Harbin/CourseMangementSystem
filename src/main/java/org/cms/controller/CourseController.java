3bbcae503317461627520ae1bbba7631d3a7d836
Li-Zhenghao-Harbin <dream7c@126.com>
2025-09-29 00:32:53 +0800

    调试

--- src/main/java/org/cms/controller/CourseController.java
+++ src/main/java/org/cms/controller/CourseController.java
@@ -1,4 +1,11 @@
 package org.cms.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("Course")
+@RequestMapping("/course")
+@CrossOrigin(allowCredentials = "true", allowedHeaders = "*", originPatterns = "*")
 public class CourseController {
 }
